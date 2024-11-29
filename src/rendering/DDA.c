12885c047a2c04fb01535b2c5063a4c842700b86
Rodrigo Cruz Antunes <rcruz-an@c2r3s3.42porto.com>
2024-11-29 15:21:57 +0000

    rewrite some c files

---
+++ src/rendering/DDA.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   DDA.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcruz-an <rcruz-an@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/20 16:00:49 by joaosilva         #+#    #+#             */
+/*   Updated: 2024/11/29 14:24:51 by rcruz-an         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/cub3d.h"
+
+void hit_checker(t_game *game, int *side /*, ... ... */)
+{
+    
+}
+
+static void hit_length(t_game *game, int side)
+{
+    
+}
+
+static void draw_walls_data(t_game *game, t_game )
+{
+    
+}
+
+/* DDA --> Digital Differential Analysis */
+static void dda_calculations(t_game *game)
+{
+    // Usar raycasting para calcular a parede e desenhar as texturas
+}

5d276c90066be02483b8b20201c0d2f19f2eedfd
Rodrigo Cruz Antunes <rcruz-an@c2r3s5.42porto.com>
2024-12-03 11:00:26 +0000

    added comments

--- src/rendering/draw.c
+++ src/rendering/draw.c
@@ -6,7 +6,7 @@
 /*   By: rcruz-an <rcruz-an@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 15:59:19 by joaosilva         #+#    #+#             */
-/*   Updated: 2024/12/03 10:27:26 by rcruz-an         ###   ########.fr       */
+/*   Updated: 2024/12/03 10:44:22 by rcruz-an         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,16 +48,17 @@ static void draw_background(t_game *game)
     }
 }
 
+/* make it darker the further it is from the player */
 int darken_color(int color, double perp_wall_dist)
 {
  	int	r;
 	int	g;
 	int	b;
 
-	perp_wall_dist /= 5;
+	perp_wall_dist /= 5; //Make it darker, not change colors
 	if (perp_wall_dist < 1)
 		return (color);
-	r = (color >> 16) / perp_wall_dist;
+	r = (color >> 16) / perp_wall_dist; //Bitwise to reach the red color (0xRRGGBB)
 	g = (color >> 8 & 0xFF) / perp_wall_dist;
 	b = (color & 0xFF) / perp_wall_dist;
 	return (r << 16 | g << 8 | b);   
@@ -69,7 +70,7 @@ void init_draw(t_game *game)
     int i;
 
     i = -1;
-    while (++i < 4) 
+    while (++i < 4) //Get the address of the textures
 	{
 		game->textures[i].addr = mlx_get_data_addr(game->textures[i].img, 
 			&game->textures[i].bits_per_pixel, &game->textures[i].len,
@@ -103,11 +104,11 @@ void draw(t_game *game, int x)
 	y = -1;
 	while (y < game->draw.end)
 	{
-		tex_y = (int)game->draw.pos & (TEXTURE_HEIGHT - 1);
+		tex_y = (int)game->draw.pos & (TEXTURE_HEIGHT - 1); //update texture_y until it reaches the end of the texture
 		game->draw.pos += game->draw.step;
-		color = get_pixil(&game->textures[game->ray.reached_wall], game->ray.hit, tex_y);
-		color = darken_color(color, game->ray.perp_wall_dist);
-		put_pixil(&game->pixels, x, y, color);
+		color = get_pixil(&game->textures[game->ray.reached_wall], game->ray.hit, tex_y); //get the address of the pixel
+		color = darken_color(color, game->ray.perp_wall_dist); //Make it darker to be more realistic
+		put_pixil(&game->pixels, x, y, color); //put pixel (it is printed in raycasting)
 		y++;
 	}
 }
